676de4b8d9a6a79888e75431211ce2a75940d656
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-03-23 19:39:19 -0300

    organizing project structure and starting server

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import http, { request } from 'node:http'
+
+const PORT = process.env.PORT || 3000
+
+const server = http.createServer((request, response) => {
+    response.end('Hello World')
+}).listen(PORT, () => console.log(`server is running at ${PORT}`))
+
+export{
+    server
+}
