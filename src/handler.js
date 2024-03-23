c5c54d5563554f7b763764684a9a0bc1ce144c47
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-03-23 20:01:49 -0300

    parsing URL

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,15 @@
+import {parse} from 'node:url'
+
 function handler(request, response){
+    const {
+        url,
+        method
+    } = request
+    const {
+        pathname
+    } = parse(url, true)
+    const key = `${pathname}:${method.toLowerCase()}`
+    console.log({key})
     response.end('Hello World')
 }
 
