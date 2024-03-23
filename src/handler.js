c5fb5a9a81621614e94bc4571f795dcd0cc80618
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-03-23 20:15:19 -0300

    Configuring handler

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,18 @@
 import {parse} from 'node:url'
+import { DEFAULT_HEADER } from './util/util.js'
+
+const allRoutes = {
+    '/heroes:get': (request, response) => {
+        response.write('GET')
+        response.end()
+    },
+    //404 routes
+    default: (request, response) => {
+        response.writeHead(404, DEFAULT_HEADER)
+        response.write('not found')
+        response.end()
+    }
+}
 
 function handler(request, response){
     const {
@@ -9,7 +23,8 @@ function handler(request, response){
         pathname
     } = parse(url, true)
     const key = `${pathname}:${method.toLowerCase()}`
-    console.log({key})
+    const chosen = allRoutes[key] || allRoutes.default
+    return chosen(request, response)
     response.end('Hello World')
 }
 
